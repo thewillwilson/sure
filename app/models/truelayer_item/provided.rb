6ce8b4cc56ed5e9b69c0af0a518a6685be4775e5
Will Wilson <will@willwilson.uk>
2026-04-29 17:56:47 +0100

    fix: use token_lapsed? for UI warnings, keep token_expired? for proactive refresh only

--- app/models/truelayer_item/provided.rb
+++ app/models/truelayer_item/provided.rb
@@ -24,6 +24,10 @@ module TruelayerItem::Provided
     access_token.present? && token_expires_at.present? && token_expires_at <= 5.minutes.from_now
   end
 
+  def token_lapsed?
+    access_token.present? && token_expires_at.present? && token_expires_at <= Time.current
+  end
+
   def refresh_tokens!
     with_lock do
       provider = Provider::Truelayer.new(
